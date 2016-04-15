b6a32220689fd6ec0060852ea73fdfe03840ad57
liukx08 <liu.kexi.1990@gmail.com>
2016-04-14 18:14:46 -0700

    PQ3 finished

--- PQ3/cut.cpp
+++ PQ3/cut.cpp
@@ -4,8 +4,8 @@
 #include<string>
 #include<sstream>
 #include<ctime>
-#define SIZE 8
-#define ITERATIONS 50
+#define SIZE 200
+#define ITERATIONS 1000
 
 using namespace std;
 
@@ -21,6 +21,23 @@ class Vertex {
     Vertex *Vnext;
     Edge *first;
 } *head; 
+
+void output(){
+    Vertex *vp=head;
+    Edge *ep=head->first;
+    cout<<"---------------------------------------------------------------------"<<endl;
+    while(vp!=NULL){
+      cout << vp->Vname << '\t';
+      while(ep!=NULL){
+        cout << ep->Ename << '\t';
+        ep=ep->Enext;
+      }
+      cout << endl;
+      vp=vp->Vnext;
+      if(vp!=NULL)ep=vp->first;
+    }
+    cout<<"---------------------------------------------------------------------"<<endl;
+}
 
 void readGroup(int *nedge,char *fileName) {
   FILE *data=fopen(fileName,"r");
@@ -75,43 +92,113 @@ void getEdge(int m,Vertex* &node1,Vertex* &node2) {
       r--;
     }
   }
+  if(theEdge==NULL){
+      node1=node1->Vnext;
+      theEdge=node1->first;
+  }
   node2=head;
   while(node2->Vname!=theEdge->Ename)node2=node2->Vnext;
 }
 
-void mergeVertex(Vertex *v1, Vertex *v2) {
+void mergeVertex(int *m, Vertex* &v1, Vertex* &v2) {
   Vertex *merged=new Vertex;
   merged->Vname=v1->Vname+','+v2->Vname;
   merged->Vnext=head;
   merged->first=NULL;
   head=merged;
-  Edge *temp=v1->first;
-  Edge *dump=v1->first;
-  while(temp!=NULL){
-    if(temp->Ename==v2->Vname){
-      dump=temp;
-      temp
+  Edge *tail=v1->first;
+  Edge *sweep=v1->first->Enext;
+  Edge *temp=NULL;
+/*delete v2 edges in v1*/
+  while(sweep!=NULL){
+    if(sweep->Ename==v2->Vname){
+      temp=sweep;
+      sweep=sweep->Enext;
+      (*m)--;
+      delete temp;
+      temp=NULL;
+    }
+    else {
+      tail->Enext=sweep;
+      tail=tail->Enext;
+      sweep=sweep->Enext;
+    }
+  }
+  tail->Enext=NULL;
+  if(v1->first->Ename==v2->Vname){
+    temp=v1->first;
+    v1->first=v1->first->Enext;
+    (*m)--;
+    delete temp;
+    temp=NULL;
+  }
+/*delete v1 edges in v2*/
+  tail=v2->first;
+  sweep=v2->first->Enext;
+  while(sweep!=NULL){
+    if(sweep->Ename==v1->Vname){
+      temp=sweep;
+      sweep=sweep->Enext;
+      (*m)--;
+      delete temp;
+      temp=NULL;
+    }
+    else {
+      tail->Enext=sweep;
+      tail=tail->Enext;
+      sweep=sweep->Enext;
+    }
+  }
+  tail->Enext=NULL;
+  if(v2->first->Ename==v1->Vname){
+    temp=v2->first;
+    v2->first=v2->first->Enext;
+    (*m)--;
+    delete temp;
+    temp=NULL;
+  }
+  merged->first=v1->first;
+  tail=v1->first;
+  if(tail==NULL)merged->first=v2->first;
+  else{
+    while(tail->Enext!=NULL)tail=tail->Enext;
+    tail->Enext=v2->first;
+  }
+  Vertex *NodeSweep=head;
+  while(NodeSweep->Vnext->Vname!=v1->Vname)NodeSweep=NodeSweep->Vnext;
+  NodeSweep->Vnext=NodeSweep->Vnext->Vnext;
+  NodeSweep=head;
+  while(NodeSweep->Vnext->Vname!=v2->Vname)NodeSweep=NodeSweep->Vnext;
+  NodeSweep->Vnext=NodeSweep->Vnext->Vnext;
+  sweep=merged->first;
+  while(sweep!=NULL){
+    NodeSweep=head;
+    while(NodeSweep->Vname!=sweep->Ename)NodeSweep=NodeSweep->Vnext;
+    temp=NodeSweep->first;
+    while(temp!=NULL){if((temp->Ename==v1->Vname)||(temp->Ename==v2->Vname))temp->Ename=merged->Vname;temp=temp->Enext;}
+    sweep=sweep->Enext;
+  }
+  delete v1;v1=NULL;
+  delete v2;v2=NULL;
 }
 
+
 int main(int argc, char **argv) {
-  int m=0;
-  readGroup(&m,"./data");
-  Vertex *vp=head;
-  Edge *ep=head->first;
-  while(vp!=NULL){
-    cout << vp->Vname << '\t';
-    while(ep!=NULL){
-      cout << ep->Ename << '\t';
-      ep=ep->Enext;
+  int min_cut=SIZE;
+  int l,k;
+  for(l=1;l<=ITERATIONS;l++){
+    int m=0;
+    srand(l);
+    readGroup(&m,"./karger.txt");
+//    output();
+    for(k=0;k<SIZE-2;k++){
+      Vertex *N1=NULL;
+      Vertex *N2=NULL;
+      getEdge(m,N1,N2);
+      mergeVertex(&m,N1,N2);
     }
-    cout << endl;
-    vp=vp->Vnext;
-    if(vp!=NULL)ep=vp->first;
+    if(min_cut>(m/2))min_cut=m/2;
   }
-  cout << m << endl;
-  Vertex *N1=head;
-  Vertex *N2=head;
-  getEdge(m,N1,N2);
-  cout << N1->Vname << "  " << N2->Vname << endl;
+  cout<<min_cut<<endl;
   return 0;
 }
